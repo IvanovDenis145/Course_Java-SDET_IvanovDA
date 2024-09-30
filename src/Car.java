b705f882cb2a74fd7b89ac8d5257916dce411a54
145Game <ivanovdenis98040@gmail.com>
2024-09-30 21:36:30 +0400

    Branch 1

---
+++ src/Car.java
@@ -0,0 +1,28 @@
+public class Car extends Vehicle implements Electric{
+    private int doors;
+    private boolean allEnergy;
+    public Car(String brand, String model, int doors) {
+        super(brand, model);
+        this.doors = doors;
+        this.allEnergy = false;
+    }
+
+    @Override
+    public void chargedBattery() {
+        allEnergy = true;
+        System.out.println("Батарея автомобиля полностью заряжена");
+    }
+
+    @Override
+    public void Readiness() {
+        if(allEnergy){
+            System.out.println("Автомобиль готов к работе");
+        }
+        else{
+            System.out.println("Автомобиль не готов к работе");
+        }
+    }
+    public void displayDoors() {
+        System.out.println("Число дверей : " + doors);
+    }
+}
